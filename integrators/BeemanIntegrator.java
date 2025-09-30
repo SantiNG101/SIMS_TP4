b71b9a6525067efbdfe39786920ee14c164c73b7
solrodriguezg <solrodriguez@itba.edu.ar>
2025-09-30 19:07:46 -0300

    fix on beeman

--- integrators/BeemanIntegrator.java
+++ integrators/BeemanIntegrator.java
@@ -3,6 +3,12 @@ public class BeemanIntegrator implements Integrator {
     public void step(Particle[] p, double dt, ForceCalculator fc) {
         int n = p.length;
 
+        // --- Guardar aceleraciones actuales (a_n) para cada partícula ---
+        Vector[] a_n = new Vector[n];
+        for (int i = 0; i < n; i++) {
+            a_n[i] = new Vector(p[i].a.x, p[i].a.y, p[i].a.z); // copia
+        }
+
         // --- Predicción de posiciones ---
         for (int i = 0; i < n; i++) {
             Particle pi = p[i];
@@ -19,15 +25,19 @@ public class BeemanIntegrator implements Integrator {
         // --- Corrección de velocidades ---
         for (int i = 0; i < n; i++) {
             Particle pi = p[i];
-            Vector a_next = pi.a; // ya calculada con posiciones nuevas
+            Vector a_next = new Vector(pi.a.x, pi.a.y, pi.a.z);  // a_{n+1} (copia)
+            Vector a_curr = a_n[i];                               // a_n (copia guardada)
+            Vector a_prev = new Vector(pi.a_prev.x, pi.a_prev.y, pi.a_prev.z); // a_{n-1}
+
+            // v_{n+1} = v_n + dt/6 (2 a_{n+1} + 5 a_n - a_{n-1})
             Vector vnew = pi.v
-                    .add(a_next.mul(dt / 3.0))
-                    .add(pi.a.mul(5.0 * dt / 6.0))
-                    .sub(pi.a_prev.mul(dt / 6.0));
+                    .add(a_next.mul((2.0 * dt) / 6.0))
+                    .add(a_curr.mul((5.0 * dt) / 6.0))
+                    .sub(a_prev.mul((1.0 * dt) / 6.0));
 
             // actualizar v y almacenar a_prev para próximo paso
             pi.v.set(vnew);
-            pi.a_prev.set(pi.a);
+            pi.a_prev.set(a_curr);
         }
     }
 
